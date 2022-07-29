20f7d86e97ec63c5a9f9725d4c6e08aa50800607
Cyril Hue <cy.hue80@gmail.com>
2022-07-30 00:53:48 +0200

    Update data logic for build
    Update github workflow

---
+++ src/_data/services.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const fetch = require("node-fetch");
+const STAPIURL = `https://cms-api-dev.herokuapp.com`;
+
+module.exports = async () => {
+  const result = await fetch(`${STAPIURL}/api/services`)
+    .catch((err) => {
+      console.error(err);
+    })
+    .then((res) => res.json());
+  return result.data;
+};
---
+++ src/about.md
@@ -0,0 +1,8 @@
+---
+title: About page
+layout: base.njk
+---
+
+## This is about pages
+
+J'ai pas grand chose à dire
