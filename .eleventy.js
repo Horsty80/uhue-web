b38e3bf580d814bacecfa6e7418fb0ed38d7e3de
Cyril Hue <cy.hue80@gmail.com>
2022-07-31 10:21:33 +0200

    Start design the website

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,23 @@
+
+const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function (eleventyConfig) {
+  // Merge data instead of overriding
+  eleventyConfig.setDataDeepMerge(true);
+
+  // human readable date
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy"
+    );
+  });
+  // Syntax Highlighting for Code blocks
+  eleventyConfig.addPlugin(syntaxHighlight);
+
   eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/static/img");
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
   eleventyConfig.addWatchTarget("./src/css/");
 
   return {
@@ -7,5 +25,6 @@ module.exports = function (eleventyConfig) {
       input: "src",
       output: "public",
     },
+    htmlTemplateEngine: "njk",
   };
 };
