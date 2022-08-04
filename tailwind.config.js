30dec781672f0d0e2600b7aeb060e55b8adfce1d
Cyril Hue <cy.hue80@gmail.com>
2022-08-04 23:40:50 +0200

    Add style to navbar on responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
       colors: {
         "uhue-gray": "#E3ECF4",
         "uhue-dark": "#0E1531",
-        "uhue-indigo": "#7416EC",
+        "uhue-indigo": "#6366f1",
+        "uhue-indigo-pop": "#7416EC",
         "uhue-bluegreen": "#0F8094",
         "uhue-lightblue": "#00D9FE",
       },
