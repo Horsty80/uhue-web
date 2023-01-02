2d467efc3a3434b7e09108a262845559156e82be
Cyril Hue <cy.hue80@gmail.com>
2023-01-02 01:22:06 +0100

    Update data with slug

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const API_URL = process.env.API_URL;
 
 module.exports = async () => {
-  const result = await fetch(`${API_URL}posts?fields=*,related_universe_id.title,author.pseudo`)
+  const result = await fetch(`${API_URL}posts?fields=*,related_universe_id.slug,author.pseudo`)
     .catch((err) => {
       console.error(err);
     })
