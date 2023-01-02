50444f82b720e81264735ffc655a5ebffff761f7
Cyril Hue <cy.hue80@gmail.com>
2023-01-02 01:51:47 +0100

    Wrong var using

--- src/_data/universes.js
+++ src/_data/universes.js
@@ -1,11 +1,11 @@
 const fetch = require("node-fetch");
 require("dotenv").config();
 
-const STRAPI_URL = process.env.API_URL;
+const API_URL = process.env.API_URL;
 
 console.log("Fetch API :", API_URL);
 module.exports = async () => {
-  const result = await fetch(`${STRAPI_URL}/universes`)
+  const result = await fetch(`${API_URL}/universes`)
     .catch((err) => {
       console.error(err);
     })
